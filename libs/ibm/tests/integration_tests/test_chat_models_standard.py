7ee61bf7a5040b5908ae98cea659d4760d90b9e9
Mateusz Szewczyk <Mateusz.Szewczyk@ibm.com>
2024-10-18 15:07:25 +0200

    Update standard chat tests

--- libs/ibm/tests/integration_tests/test_chat_models_standard.py
+++ libs/ibm/tests/integration_tests/test_chat_models_standard.py
@@ -23,7 +23,7 @@ class TestChatWatsonxStandard(ChatModelIntegrationTests):
 
     @property
     def supports_image_inputs(self) -> bool:
-        return False
+        return False  # Supported, but set False due to token limit in test account
 
     @property
     def returns_usage_metadata(self) -> bool:
@@ -61,12 +61,6 @@ class TestChatWatsonxStandard(ChatModelIntegrationTests):
             "apikey": WX_APIKEY,
             "project_id": WX_PROJECT_ID,
         }
-
-    @pytest.mark.xfail(reason="Supported for vision model.")
-    def test_image_inputs(self, model: BaseChatModel) -> None:
-        model.watsonx_model._inference.model_id = MODEL_ID_IMAGE  # type: ignore[attr-defined]
-        super().test_image_inputs(model)
-        model.watsonx_model._inference.model_id = MODEL_ID  # type: ignore[attr-defined]
 
     @pytest.mark.xfail(reason="Not implemented tool_choice as `any`.")
     def test_structured_few_shot_examples(self, model: BaseChatModel) -> None:
