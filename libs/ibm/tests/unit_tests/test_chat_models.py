e215a3b242496db23cb947be85a8b3c1ae1cdfc5
Mateusz Świtała <151041464+Mateusz-Switala@users.noreply.github.com>
2025-02-14 10:30:56 +0100

    fix docstring and clean up (#53)
    * fix docstring and clean up
    
    * fix linting
    
    * fix linter

--- libs/ibm/tests/unit_tests/test_chat_models.py
+++ libs/ibm/tests/unit_tests/test_chat_models.py
@@ -1,6 +1,7 @@
 """Test ChatWatsonx API wrapper."""
 
 import os
+from typing import Any
 
 from langchain_ibm import ChatWatsonx
 
@@ -82,3 +83,55 @@ def test_initialize_chat_watsonx_cpd_bad_path_without_instance_id() -> None:
     except ValueError as e:
         assert "instance_id" in e.__str__()
         assert "WATSONX_INSTANCE_ID" in e.__str__()
+
+
+def test_initialize_chat_watsonx_with_all_supported_params(mocker: Any) -> None:
+    # All params values are taken from
+    # ibm_watsonx_ai.foundation_models.schema.TextChatParameters.get_sample_params()
+
+    from ibm_watsonx_ai.foundation_models.schema import (  # type: ignore[import-untyped]
+        TextChatParameters,
+    )
+
+    TOP_P = 0.8
+
+    def mock_modelinference_chat(*args: Any, **kwargs: Any) -> dict:
+        """Mock ModelInference.chat method"""
+
+        assert kwargs.get("params", None) == (
+            TextChatParameters.get_sample_params()
+            | dict(
+                logit_bias={"1003": -100, "1004": -100}, seed=41, stop=["this", "the"]
+            )
+            | dict(top_p=TOP_P)
+        )
+        # logit_bias, seed and stop available in sdk since 1.2.7
+        return {"id": "123", "choices": [{"message": dict(content="Hi", role="ai")}]}
+
+    with mocker.patch(
+        "ibm_watsonx_ai.foundation_models.ModelInference.__init__", return_value=None
+    ), mocker.patch(
+        "ibm_watsonx_ai.foundation_models.ModelInference.chat",
+        side_effect=mock_modelinference_chat,
+    ):
+        chat = ChatWatsonx(
+            model_id="google/flan-ul2",
+            url="https://us-south.ml.cloud.ibm.com",  # type: ignore[arg-type]
+            apikey="test_apikey",  # type: ignore[arg-type]
+            frequency_penalty=0.5,
+            logprobs=True,
+            top_logprobs=3,
+            presence_penalty=0.3,
+            response_format={"type": "json_object"},
+            temperature=0.7,
+            max_tokens=100,
+            time_limit=600000,
+            top_p=0.9,
+            n=1,
+            logit_bias={"1003": -100, "1004": -100},
+            seed=41,
+            stop=["this", "the"],
+        )
+
+        # change only top_n
+        chat.invoke("Hello", top_p=TOP_P)
