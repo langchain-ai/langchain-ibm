1efba298ec445094856bf10975e48e5bb42db0cb
Mateusz Szewczyk <Mateusz.Szewczyk@ibm.com>
2025-03-12 11:21:37 +0100

    fix: Fixed init ModelInference with deployment_id and watsonx_client

--- libs/ibm/langchain_ibm/chat_models.py
+++ libs/ibm/langchain_ibm/chat_models.py
@@ -623,6 +623,7 @@ class ChatWatsonx(BaseChatModel):
         if isinstance(self.watsonx_client, APIClient):
             watsonx_model = ModelInference(
                 model_id=self.model_id,
+                deployment_id=self.deployment_id,
                 params=self.params,
                 api_client=self.watsonx_client,
                 project_id=self.project_id,
