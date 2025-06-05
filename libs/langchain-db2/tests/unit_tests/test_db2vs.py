c39ae64dbd0fa178c5a845b730d88b85c79163cd
Chester Curme <chester.curme@gmail.com>
2025-06-05 09:21:04 -0400

    add a test

---
+++ libs/langchain-db2/tests/unit_tests/test_db2vs.py
@@ -0,0 +1,15 @@
+from unittest.mock import MagicMock
+
+from langchain_core.embeddings.fake import DeterministicFakeEmbedding
+
+from langchain_db2.db2vs import DB2VS
+
+def test_init() -> None:
+    """Test that the DB2VS class can be initialized."""
+    client = MagicMock()
+    embedding = DeterministicFakeEmbedding(size=100)
+    table_name = "foo"
+    db2vs = DB2VS(client, embedding, table_name)
+    assert db2vs is not None
+    assert isinstance(db2vs, DB2VS)
+    assert len(client.mock_calls) == 3
