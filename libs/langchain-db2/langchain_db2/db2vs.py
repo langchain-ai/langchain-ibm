2fa459a3e98de02c70ecbfe55014b05a46a560cd
YIWEI-HE <YIWEI.HE@ibm.com>
2025-07-02 14:49:34 -0700

    Enhancement: initialize DB2VS with connection_args.

--- libs/langchain-db2/langchain_db2/db2vs.py
+++ libs/langchain-db2/langchain_db2/db2vs.py
@@ -187,7 +187,7 @@ class DB2VS(VectorStore):
 
     def __init__(
         self,
-        client: Connection,
+        client: Optional[Connection],
         embedding_function: Union[
             Callable[[str], List[float]],
             Embeddings,
@@ -196,7 +196,21 @@ class DB2VS(VectorStore):
         distance_strategy: DistanceStrategy = DistanceStrategy.EUCLIDEAN_DISTANCE,
         query: Optional[str] = "What is a Db2 database",
         params: Optional[Dict[str, Any]] = None,
+        connection_args: Optional[Dict[str, Any]] = None,
     ):
+        if client is None:
+            if connection_args is None:
+                database=connection_args.get("database")
+                host=connection_args.get("host")
+                port=connection_args.get("port")
+                username=connection_args.get("username")
+                password=connection_args.get("password")
+
+                conn_str=f"DATABASE={database};hostname={host};port={port};uid={username};pwd={password};"
+                self.client = ibm_db_dbi.connect(conn_str, '', '')
+            else:
+                raise RuntimeError("No valid connection or connection_args is passed")
+        
         try:
             """Initialize with ibm_db_dbi client."""
             self.client = client
