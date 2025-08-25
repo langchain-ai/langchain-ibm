4e3110ac36d14bbc27035def0eed9d8acebf5e50
Wojciech-Rebisz <Wojciech.Rebisz@ibm.com>
2025-08-25 13:21:27 +0200

    Add unit tests for toolkit CPD

--- libs/ibm/tests/unit_tests/test_toolkit.py
+++ libs/ibm/tests/unit_tests/test_toolkit.py
@@ -2,6 +2,8 @@
 
 import os
 
+import pytest
+
 from langchain_ibm import WatsonxToolkit
 
 os.environ.pop("WATSONX_APIKEY", None)
@@ -31,10 +33,50 @@ def test_initialize_watsonx_toolkit_cloud_bad_api_key() -> None:
         assert "WATSONX_APIKEY" in e.__str__() and "WATSONX_TOKEN" in e.__str__()
 
 
-def test_initialize_watsonx_toolkit_cpd_invalid_credentials() -> None:
-    try:
-        WatsonxToolkit(url="https://cpd-cpd-instance.apps.sample.cp.fyre.ibm.com")  # type: ignore[arg-type]
-    except ValueError as e:
-        assert "username" in e.__str__()
-        assert "password" in e.__str__()
-        assert "instance_id" in e.__str__()
+def test_initialize_watsonx_toolkit_cpd_bad_path_without_all() -> None:
+    with pytest.raises(ValueError) as e:
+        WatsonxToolkit(
+            url="https://cpd-zen.apps.cpd48.cp.fyre.ibm.com",  # type: ignore[arg-type]
+        )
+    assert (
+        "apikey" in str(e.value)
+        and "password" in str(e.value)
+        and "token" in str(e.value)
+    )
+    assert (
+        "WATSONX_APIKEY" in str(e.value)
+        and "WATSONX_PASSWORD" in str(e.value)
+        and "WATSONX_TOKEN" in str(e.value)
+    )
+
+
+def test_initialize_watsonx_toolkit_cpd_bad_path_password_without_username() -> None:
+    with pytest.raises(ValueError) as e:
+        WatsonxToolkit(
+            url="https://cpd-zen.apps.cpd48.cp.fyre.ibm.com",  # type: ignore[arg-type]
+            password="test_password",  # type: ignore[arg-type]
+        )
+    assert "username" in str(e.value)
+    assert "WATSONX_USERNAME" in str(e.value)
+
+
+def test_initialize_watsonx_toolkit_cpd_bad_path_apikey_without_username() -> None:
+    with pytest.raises(ValueError) as e:
+        WatsonxToolkit(
+            url="https://cpd-zen.apps.cpd48.cp.fyre.ibm.com",  # type: ignore[arg-type]
+            apikey="test_apikey",  # type: ignore[arg-type]
+        )
+
+    assert "username" in str(e.value)
+    assert "WATSONX_USERNAME" in str(e.value)
+
+
+def test_initialize_watsonx_toolkit_cpd_bad_path_without_instance_id() -> None:
+    with pytest.raises(ValueError) as e:
+        WatsonxToolkit(
+            url="https://cpd-zen.apps.cpd48.cp.fyre.ibm.com",  # type: ignore[arg-type]
+            apikey="test_apikey",  # type: ignore[arg-type]
+            username="test_user",  # type: ignore[arg-type]
+        )
+    assert "instance_id" in str(e.value)
+    assert "WATSONX_INSTANCE_ID" in str(e.value)
