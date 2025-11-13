d90697ffb0b2417c33eb78a523d21477a0587199
Christophe Bornet <cbornet@hotmail.com>
2025-11-13 12:18:03 +0100

    chore(langchain-ibm): improve ruff rules TC (#149)

--- libs/ibm/langchain_ibm/chat_models.py
+++ libs/ibm/langchain_ibm/chat_models.py
@@ -7,9 +7,9 @@ import contextlib
 import json
 import logging
 import warnings
-from collections.abc import AsyncIterator, Callable, Iterator, Mapping, Sequence
 from operator import itemgetter
 from typing import (
+    TYPE_CHECKING,
     Any,
     Literal,
     cast,
@@ -24,11 +24,6 @@ from ibm_watsonx_ai.foundation_models.schema import (  # type: ignore[import-unt
     TextChatParameters,
 )
 from ibm_watsonx_ai.gateway import Gateway  # type: ignore[import-untyped]
-from langchain_core.callbacks import (
-    AsyncCallbackManagerForLLMRun,
-    CallbackManagerForLLMRun,
-)
-from langchain_core.language_models import LanguageModelInput
 from langchain_core.language_models.chat_models import (  # type: ignore[attr-defined]
     BaseChatModel,
     LangSmithParams,
@@ -68,7 +63,6 @@ from langchain_core.runnables import (
     RunnableMap,
     RunnablePassthrough,
 )
-from langchain_core.tools import BaseTool
 from langchain_core.utils._merge import merge_dicts
 from langchain_core.utils.function_calling import (
     convert_to_openai_function,
@@ -99,6 +93,16 @@ from langchain_ibm.utils import (
     resolve_watsonx_credentials,
     secret_from_env_multi,
 )
+
+if TYPE_CHECKING:
+    from collections.abc import AsyncIterator, Callable, Iterator, Mapping, Sequence
+
+    from langchain_core.callbacks import (
+        AsyncCallbackManagerForLLMRun,
+        CallbackManagerForLLMRun,
+    )
+    from langchain_core.language_models import LanguageModelInput
+    from langchain_core.tools import BaseTool
 
 logger = logging.getLogger(__name__)
 
